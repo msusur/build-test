2dc0ffb8cfb05d67158cb33ce09ba0572300d52a
Mert Susur <mail@mertsusur.com>
2013-05-16 15:52:11 +0300

    nuget and cmd files update

--- BuildTest/Properties/AssemblyInfo.cs
+++ BuildTest/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BuildTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Build Test Operation")]
+[assembly: AssemblyDescription("Build Test Assembly is awesome")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Msusur")]
 [assembly: AssemblyProduct("BuildTest")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("MSUSUR")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0303bd80-cee2-4bc4-91b4-dd2c5628b03e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
---
+++ Get-Clean-Build-Test-Package.cmd
@@ -0,0 +1,4 @@
+git clone git://github.com/msusur/build-test.git
+clean.cmd
+build.cmd
+package.cmd
