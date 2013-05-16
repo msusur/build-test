1fb35200ce8be953f9f756360c37d29876096e5f
Mert Susur <mail@mertsusur.com>
2013-05-16 15:16:00 +0300

    test fix

---
+++ BuildTest.Tests/BuildTestClassTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace BuildTest.Tests
+{
+    // ReSharper disable InconsistentNaming
+    public class BuildTestClassTest
+    {
+        [Fact]
+        public void Test_That_Fails()
+        {
+            Assert.True(new TestClass().False());
+        }
+
+        [Fact]
+        public void Test_That_Passes()
+        {
+            Assert.True(new TestClass().True());
+        }
+    }
+}
